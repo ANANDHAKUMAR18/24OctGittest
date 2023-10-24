13cb0336029414bafad6efbb987ac29080b550af
Sandhiya0205 <146726602+Sandhiya0205@users.noreply.github.com>
2023-10-24 19:58:03 +0530

    Update Maths.java

--- src/test/java/git/Maths.java
+++ src/test/java/git/Maths.java
@@ -8,10 +8,16 @@ public class Maths {
 		int result =a+b;
 		System.out.println("Sum of two numbers =" + result);
 		}
+
+	public void M2(){
+		
+		System.out.println("Sum of two numbers");
+		}
 	
 	
 	public static void main(String[] args){
         Maths obj=new Maths();
 	obj.add(3,0);
+        obj.M2();
 	}
 }
